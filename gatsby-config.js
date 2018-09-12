038c430af9528b92e6e81152883977203c241694
Mark Myers <markalope@gmail.com>
2018-09-12 14:26:07 -0700

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `8iy48macnh5d`,
+        accessToken: `1b10f6c7613241668d0f762bd64497e44b0738cd3b2eee77182dce7ef3874ded`,
+      },
+    }
+],
+}
