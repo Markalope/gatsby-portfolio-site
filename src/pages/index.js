775402fb0ea186aaf14b05402a69a189a99ecfba
Mark Myers <markalope@gmail.com>
2018-09-13 12:52:06 -0700

    finished transferring content from static HTML site

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,6 +18,17 @@ export default ({data}) =>
     <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet" />
     <title>Mark Myers :: Javascript Developer in Portland OR</title>
   </Helmet>
+
+  <section className={mainStyles.topnavbar}>
+        <div className={mainStyles.iconslinks}> <p>Mark Myers :: Javascript Developer in Portland, OR &nbsp; &nbsp; 
+      
+          <a target="_blank" href="https://twitter.com/markalope" title="Link to Markalope on Twitter"><i className="fab fa-twitter"></i></a>
+          <a target="_blank" href="https://www.linkedin.com/in/markalope" title="Link to Mark Myers on LinkedIn"><i className="fab fa-linkedin"></i></a>
+          <a target="_blank" href="https://github.com/markalope" title="Link to Mark Myers GitHub Profile"><i className="fab fa-github"></i></a>
+          <a target="_blank" href="mailto:markalope@gmail.com" title="Email link for Markalope@gmail.com"><i className="fas fa-envelope-square"></i></a>
+        </p></div>
+  </section>
+
   <section className={mainStyles.header}>
   <div className={mainStyles.headercontents}>
   <h1>{data.allContentfulBio.edges[0].node.title}</h1>
@@ -27,7 +38,6 @@ export default ({data}) =>
       <a target="_blank" href="https://www.linkedin.com/in/markalope" title="Link to Mark Myers on LinkedIn"><i className="fab fa-linkedin"></i></a>
       <a target="_blank" href="https://github.com/markalope" title="Link to Mark Myers GitHub Profile"><i className="fab fa-github"></i></a>
       <a target="_blank" href="mailto:markalope@gmail.com" title="Email link for Markalope@gmail.com"><i className="fas fa-envelope-square"></i></a>
-       
       </div>
     </div>  
 </section>
@@ -35,8 +45,80 @@ export default ({data}) =>
 <section className={mainStyles.bio}>
     <p>{data.allContentfulBio.edges[0].node.body.body}</p>
 </section>
-</div>
 
+<section className="projects">
+<div className="projectcontainer-left">
+        <div className="projecttext-left">
+          <div className="projectcontents">
+            <h3>Project Name One</h3>
+            <p>This is the short description of the project, which will display here and invite the user to click for more info.</p>
+            <p><a href="#">Click Here for More Info!</a></p>
+            <p className="techicons">
+              <i className="fab fa-react"></i> <i className="fab fa-node-js"></i>
+            </p>
+          </div>
+        </div>
+        <div className="projectimage">
+          <img src="computer-fpo.jpg" alt="screen shot of project" />      
+        </div>
+      </div> 
+      <div className="projectcontainer-right">
+          <div className="projectimage">
+              <img src="computer-fpo.jpg" alt="screen shot of project" />
+            </div>
+            <div className="projecttext-right">
+                <div className="projectcontents">
+              <h3>Project Name Two</h3>
+              <p>This is the short description of the project, which will display here and invite the user to click for more info.</p>
+              <p><a href="#">Click Here for More Info!</a></p>
+              <p className="techicons">
+                <i className="fab fa-react"></i> <i className="fab fa-node-js"></i>
+              </p>
+          </div>
+          </div>
+          
+        </div>  
+        <div className="projectcontainer-left">
+            <div className="projecttext-left">
+              <div className="projectcontents">
+                <h3>Project Name Three</h3>
+                <p>This is the short description of the project, which will display here and invite the user to click for more info.</p>
+                <p><a href="#">Click Here for More Info!</a></p>
+                <p className="techicons">
+                  <i className="fab fa-react"></i> <i className="fab fa-node-js"></i>
+                </p>
+              </div>
+            </div>
+            <div className="projectimage">
+              <img src="computer-fpo.jpg" alt="screen shot of project" />      
+            </div>
+          </div>  
+          <div className="projectcontainer-right">
+              <div className="projectimage">
+                  <img src="computer-fpo.jpg" alt="screen shot of project" />
+                </div>
+                <div className="projecttext-right">
+                    <div className="projectcontents">
+                  <h3>Project Name Four</h3>
+                  <p>This is the short description of the project, which will display here and invite the user to click for more info.</p>
+                  <p><a href="#">Click Here for More Info!</a></p>
+                  <p className="techicons">
+                    <i className="fab fa-react"></i> <i className="fab fa-node-js"></i>
+                  </p>
+              </div>
+              </div>         
+            </div>       
+
+</section>
+
+<section className={mainStyles.footer}>
+  <p>HTML/CSS | Javascript - Express, Node, Vue, React/Redux, Gatsby | Test-Driven Development (TDD) with Mocha, Chai and Jest | PostgreSQL, MongoDB with Mongoose | VSCode and CLI/BASH | Git, GitHub, Heroku, Netlify</p>
+</section>
+
+
+{/* ***** DO NOT MOVE THIS DIV - IT IS THE ENCLOSING DIV *** */}
+</div>
+{/* ***** DO NOT MOVE THIS DIV - IT IS THE ENCLOSING DIV *** */}
 
 export const query = graphql `
   query BioQuery {
