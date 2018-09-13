b712d818a5438100ac98f7e3caaf4c3abeb57675
Mark Myers <markalope@gmail.com>
2018-09-13 10:00:13 -0700

    removing dashes from class names - not supported in Gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,19 +9,19 @@ export default ({data}) =>
  <div>
    <Helmet>
             <meta charSet="utf-8" />
-            <title>This Title is coming from Helmet</title>
+            <title>Mark Myers :: Javascript Developer in Portland OR</title>
             <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet" />
 
-            </Helmet>
+    </Helmet>
   <section className={mainStyles.header}>
   <div className={mainStyles.headercontents}>
   <h1>{data.allContentfulBio.edges[0].node.title}</h1>
   <h2>{data.allContentfulBio.edges[0].node.subhead}<br/>{data.allContentfulBio.edges[0].node.subhead2}</h2>
     <div className={mainStyles.iconslinks}>
         <a target="_blank" href="https://twitter.com/markalope" title="Link to Markalope on Twitter"><i className="fab fa-twitter"></i></a>
-      <a target="_blank" href="https://www.linkedin.com/in/markalope" title="Link to Mark Myers on LinkedIn"><i class="fab fa-linkedin"></i></a>
-      <a target="_blank" href="https://github.com/markalope" title="Link to Mark Myers GitHub Profile"><i class="fab fa-github"></i></a>
-      <a target="_blank" href="mailto:markalope@gmail.com" title="Email link for Markalope@gmail.com"><i class="fas fa-envelope-square"></i></a>
+      <a target="_blank" href="https://www.linkedin.com/in/markalope" title="Link to Mark Myers on LinkedIn"><i className="fab fa-linkedin"></i></a>
+      <a target="_blank" href="https://github.com/markalope" title="Link to Mark Myers GitHub Profile"><i className="fab fa-github"></i></a>
+      <a target="_blank" href="mailto:markalope@gmail.com" title="Email link for Markalope@gmail.com"><i className="fas fa-envelope-square"></i></a>
        
       </div>
     </div>  
