f1e546fa297a363d9e7e9d3adedfd74ba7b67786
Mark Myers <markalope@gmail.com>
2018-09-12 21:04:50 -0700

    plugged HTML content in from yesterday. Replaced with contentful

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,26 @@ import  graphql from "gatsby-link"
 import typography from '../utils/typography'
 import { TypographyStyle, GoogleFont } from 'react-typography'
 
-export default ({data}) => <div><h1>Hello {data.allContentfulBio.edges[0].node.title}</h1><h2>{data.allContentfulBio.edges[0].node.subhead}</h2><p>{data.allContentfulBio.edges[0].node.body.body}</p></div>
+export default ({data}) => <div>
+  
+  <section class="header">
+  <div class="headercontents">
+  <h1>{data.allContentfulBio.edges[0].node.title}</h1>
+  <h2>{data.allContentfulBio.edges[0].node.subhead}</h2>
+    <div class="icons-links">
+        <a target="_blank" href="https://twitter.com/markalope" title="Link to Markalope on Twitter"><i class="fab fa-twitter"></i></a>
+      <a target="_blank" href="https://www.linkedin.com/in/markalope" title="Link to Mark Myers on LinkedIn"><i class="fab fa-linkedin"></i></a>
+      <a target="_blank" href="https://github.com/markalope" title="Link to Mark Myers GitHub Profile"><i class="fab fa-github"></i></a>
+      <a target="_blank" href="mailto:markalope@gmail.com" title="Email link for Markalope@gmail.com"><i class="fas fa-envelope-square"></i></a>
+       
+      </div>
+    </div>  
+</section>
+
+<section class="bio">
+    <p>{data.allContentfulBio.edges[0].node.body.body}</p>
+</section>
+</div>
 
 export const query = graphql `
   query BioQuery {
