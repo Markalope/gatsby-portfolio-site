fe00d308051b5f7fdff640d3e07555164b5f4134
Mark Myers <markalope@gmail.com>
2018-09-13 09:27:45 -0700

    YES!!!! GOT Helmet to bring in Fontawesome icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,24 @@
 import React from "react"
 import  graphql from "gatsby-link"
 import typography from '../utils/typography'
+import { Helmet } from "react-helmet"
 import { TypographyStyle, GoogleFont } from 'react-typography'
 import mainStyles from "../components/main.module.scss"
 
 export default ({data}) => 
  <div>
+   <Helmet>
+            <meta charSet="utf-8" />
+            <title>This Title is coming from Helmet</title>
+            <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet" />
+
+            </Helmet>
   <section className={mainStyles.header}>
   <div className={mainStyles.headercontents}>
   <h1>{data.allContentfulBio.edges[0].node.title}</h1>
   <h2>{data.allContentfulBio.edges[0].node.subhead}<br/>{data.allContentfulBio.edges[0].node.subhead2}</h2>
     <div className={mainStyles.iconslinks}>
-        <a target="_blank" href="https://twitter.com/markalope" title="Link to Markalope on Twitter"><i class="fab fa-twitter"></i></a>
+        <a target="_blank" href="https://twitter.com/markalope" title="Link to Markalope on Twitter"><i className="fab fa-twitter"></i></a>
       <a target="_blank" href="https://www.linkedin.com/in/markalope" title="Link to Mark Myers on LinkedIn"><i class="fab fa-linkedin"></i></a>
       <a target="_blank" href="https://github.com/markalope" title="Link to Mark Myers GitHub Profile"><i class="fab fa-github"></i></a>
       <a target="_blank" href="mailto:markalope@gmail.com" title="Email link for Markalope@gmail.com"><i class="fas fa-envelope-square"></i></a>
